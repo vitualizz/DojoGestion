b29182d536a2c6ffdab172021bf5a12b3b6d1a04
vitualizz <vitualizz@gmail.com>
2019-09-15 10:57:53 -0500

    config components and underscore implement

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'jbuilder', '~> 2.7'
 gem 'bootsnap', '>= 1.4.2', require: false
 
 group :assets do
+  gem 'sass', '~> 3.4', '>= 3.4.22'
   gem 'sass-rails', '~> 6.0'
   gem 'coffee-rails', '~> 5.0'
   gem 'uglifier', '~> 4.1'
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,17 +1,25 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
 
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+const string = require("underscore.string");
+const components = require.context("./components", true, /^\.\/.*\.js$/);
+
+import Vue from 'vue/dist/vue.esm'
+import TurbolinksAdapter from 'vue-turbolinks'
+Vue.use(TurbolinksAdapter)
+
+components.keys().forEach((component) =>{
+  component = component.match(/\w[\w.]+$/)[0]
+  name = component.match(/^\w*/)[0]
+  Vue.component(name, () => import(`./components/${component}`))
+})
+
+document.addEventListener('turbolinks:load', () => {
+  new Vue({
+    el: '#app'
+  })
+})
+

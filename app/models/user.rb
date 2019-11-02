da092bb0e5a91c845e3ff23b2d354999a14c678c
Lee Palacios <vitualizz@gmail.com>
2019-11-02 01:57:20 -0500

    login only username

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,18 +5,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  attr_writer :login
-
-  def login
-    @login || self.username || self.email
+  validates :username, uniqueness: true
+ 
+  def email_required?
+    false
   end
 
-  def self.find_for_database_authentication(warden_conditions)
-    conditions = warden_conditions.dup
-    if login = conditions.delete(:login)
-      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
-    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
-      where(conditions.to_h).first
-    end
+  def will_save_change_to_email?
+    false
   end
 end

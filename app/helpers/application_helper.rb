56cea19894051b717278b2f89872a8f93000dcdd
Lee Palacios <vitualizz@gmail.com>
2020-01-02 19:34:55 -0500

    locale files model user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,7 @@
 module ApplicationHelper
-  # Devise
-  def resource_name
-    :user
-  end
 
-  def resource
-    @resource ||= User.new
-  end
-
-  def devise_mapping
-    @devise_mapping ||= Devise.mappings[:user]
+  # I18n Internationalization
+  def t_model(field, *args)
+    t(field.to_sym, scopes: args.present? ? [:models, args] : nil)
   end
 end

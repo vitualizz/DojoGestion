b81a72db728fb0fa5ee7305a056f4fda2c935643
Lee Palacios <vitualizz@gmail.com>
2020-03-29 03:27:19 -0500

    remove action multiple

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,9 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-
-  def call_action(action, params)
-    return self.destroy if action.include?("destroy") && params.nil?
-    return self.save if action.include?("create") && params.nil?
-    self.send(action, params)
-  end
 end

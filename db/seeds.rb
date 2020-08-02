c57608dbf245092d3eef87dda03555a360677c31
Lee Palacios <vitualizz@gmail.com>
2020-08-02 02:13:58 -0500

    seeds add

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+if Rails.env.development?
+  School.create(
+    name: 'Columna Pasco',
+    subdomain: 'columna',
+    email: 'columna@school.edu.pe',
+    locale: 'es'
+  )
+
+  Student.create(
+    email: 'vitualizz@gmail.com',
+    fname: 'Lee',
+    lname: 'Palacios',
+    username: 'vitualizz',
+    document: '22886644',
+    genre: 'M'
+  )
+
+  Teacher.create(
+    email: 'nicpa@gmail.com',
+    fname: 'Nícolas',
+    lname: 'Tosirrico',
+    username: 'tosino',
+    document: '22886655',
+    genre: 'M'
+  )
+
+  Parent.create(
+    email: 'ferdinando@gmail.com',
+    fname: 'Fernando',
+    lname: 'Palacios',
+    username: 'samurai',
+    document: '22886677',
+    genre: 'M'
+  )
+end
